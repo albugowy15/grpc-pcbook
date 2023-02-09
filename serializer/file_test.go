ee8001489757795db55e24a3181eb0b36037c6e3
albugowy15 <kholidbughowi@gmail.com>
2023-02-09 10:56:08 +0700

    test: :white_check_mark: test file serializer

---
+++ serializer/file_test.go
@@ -0,0 +1,30 @@
+package serializer_test
+
+import (
+	"testing"
+
+	"github.com/albugowy15/pcbook/pb"
+	"github.com/albugowy15/pcbook/sample"
+	"github.com/albugowy15/pcbook/serializer"
+	"github.com/golang/protobuf/proto"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFileSerializer(t *testing.T) {
+	t.Parallel()
+
+	binaryFile := "../tmp/laptop.bin"
+	jsonFile := "../tmp/laptop.json"
+
+	laptop1 := sample.NewLaptop()
+	err := serializer.WriteProtobufToBinaryFile(laptop1, binaryFile)
+	require.NoError(t, err)
+
+	laptop2 := &pb.Laptop{}
+	err = serializer.ReadProtobufFromBinaryFile(binaryFile, laptop2)
+	require.NoError(t, err)
+	require.True(t, proto.Equal(laptop1, laptop2))
+
+	err = serializer.WriteProtobufToJSONFile(laptop1, jsonFile)
+	require.NoError(t, err)
+}
